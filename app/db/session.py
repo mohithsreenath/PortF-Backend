db5850b281df24a8a4f5a0349e2cfd3455263ad7
mohith-simtech <mohith.s@simtechitsolutions.in>
2026-03-06 16:05:19 +0530

    fix (error related to relationship import)

--- app/db/session.py
+++ app/db/session.py
@@ -1,14 +1,11 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-from app.core.config import DATABASE_URL
+import os
 
-engine = create_async_engine(
-    DATABASE_URL,
-    echo=True
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(DATABASE_URL, echo=True)
 
 AsyncSessionLocal = sessionmaker(
-    bind=engine,
-    class_=AsyncSession,
-    expire_on_commit=False
+    engine, class_=AsyncSession, expire_on_commit=False
 )
--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, Integer, ForeignKey, relationship
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Integer, ForeignKey
 from app.db.base import Base
 import uuid
 
