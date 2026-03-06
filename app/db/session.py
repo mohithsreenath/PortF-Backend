6aea3280f4dd1b5ec713134cadc7f624e69ecd00
mohith-simtech <mohith.s@simtechitsolutions.in>
2026-03-06 16:11:19 +0530

    fix (code)

--- app/db/session.py
+++ app/db/session.py
@@ -1,8 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-import os
+from app.core.config import DATABASE_URL
 
-DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
